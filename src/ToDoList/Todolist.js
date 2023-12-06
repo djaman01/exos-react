0f8eaee7c3cd2b8b0c139b38d84e1e1977cc9c18
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-12-06 03:28:29 +0100

    début modification input

--- src/ToDoList/Todolist.js
+++ src/ToDoList/Todolist.js
@@ -20,22 +20,32 @@ export default function Todolist() {
 
   //Quand on click qur le bouton save task
   const handleClicked = () => {
-    setClicked(true);
-    setTasks([...tasks, value]);
-    setValue("");
+    setClicked(true); //quand c'est true le task apparait
+    setTasks([...tasks, value]); //ajout de la tache dans l'array sur laquelle on va mapper
+    setValue(""); //value de l'input est vdie pour ne pas affecter la tache ajoutée
+
   }
 
+  const eraseTask = (index) => {
+    const deleteTask = tasks.filter((_, i) => i !== index) // _ =pour pouvoir utiliser le 2ème paramètre de filter qui représente l'index de l'élement, car on n'utilise pas le 1er paramètre
+    setTasks(deleteTask);
+  }
+
+  //Pour modifier le task dans un input au même endroit
+
+  const [editingIndex, setEditingIndex] = useState(null);
 
   return (
     <>
       <h1>My To do List</h1>
 
-      {clicked && tasks.map(e =>
+      {clicked && tasks.map((element, index) =>
         <div className="my-task">
-          <span>{e}</span>
+          <span className="the-task">{element}</span>
           <div className="pen-trash">
-            <FaRegTrashAlt />
             <FaRegPenToSquare />
+            <FaRegTrashAlt onClick={() => eraseTask(index)}  />
+            {/* !!! index=index element dans map // Quand on ajoute un argument pour donner une valeur au parametre de eraseTask, on est obligé d'écrire la function comme ça, sinon elle serait appelé directement quand ça va render et non quand on va click */}
           </div>
         </div>
       )}
@@ -43,6 +53,7 @@ export default function Todolist() {
       <p>Tasks: </p>
       <input type="text" value={value} onChange={handleValue} />
       <button className="btn-task" onClick={handleClicked}>Save Task</button>
+      {/* onClick={handleClicked} appelle la function quand on click, car on n'ajoute pas de paramètre */}
 
     </>
   )
