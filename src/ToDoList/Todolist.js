acedd98631b43b9a11f9ea6db724a41a1eb10175
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-12-07 20:43:45 +0100

    ajout barre quand check poru chaqeu task

--- src/ToDoList/Todolist.js
+++ src/ToDoList/Todolist.js
@@ -1,89 +1,114 @@
 import "./todolist.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { FaRegPenToSquare } from "react-icons/fa6";
 
 export default function Todolist() {
 
-  //Pour stocker la valeur de l'input
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(""); //Pour stocker la valeur de l'input
+
+  const [clicked, setClicked] = useState(false); //Pour pouvoir créer une condition que si c'est true, la task apparait
+
+  const [tasks, setTasks] = useState([]); //Pour stock une array de toutes les tasks ajoutés et mapper sur elle grpace à JSX dans return
+
+  const [editingIndex, setEditingIndex] = useState(null); //Pour suivre le task édité
+  const [editedTask, setEditedTask] = useState(""); //Cible le task édité
+
+  const [checkedTasks, setcheckedTasks] = useState([]); //Array pour suivre le checkedTasks status for each task
+
+  const updatedTasks = [...tasks];
 
   //Pour déclencher le stockage de la value de l'input en onChange
   const handleValue = (e) => {
     setValue(e.target.value)
   }
 
-  //Pour pouvoir créer une condition que si c'est true, la task apparait
-  const [clicked, setClicked] = useState(false);
-  //Pour stock une array de toutes les tasks ajoutés et mapper sur elle grpace à JSX dans return
-  const [tasks, setTasks] = useState([]);
 
   //Quand on click qur le bouton save task
   const handleClicked = () => {
     setClicked(true); //quand c'est true le task apparait
-    setTasks([...tasks, value]); //ajout de la tache dans l'array sur laquelle on va mapper
-    setValue(""); //value de l'input est vdie pour ne pas affecter la tache ajoutée
+    setTasks([...tasks, value]); //ajout de la task écrite dans l'input, à la fin de l'array qui contient toutes les tasks et sur laquelle on va mapper
+    setcheckedTasks([...checkedTasks, false]) //checkedTasks value will be false, for every new task
+    setValue(""); //value de l'input est vide pour ne pas affecter la tache ajoutée
 
   }
-
+  //Pour delete un task
   const eraseTask = (index) => {
-    const deleteTask = tasks.filter((_, i) => i !== index) // _ =pour pouvoir utiliser le 2ème paramètre de filter qui représente l'index de l'élement, car on n'utilise pas le 1er paramètre
+    const deleteTask = tasks.filter((_, i) => i !== index)
+    const deleteCheckedTasks = checkedTasks.filter((_, i) => i !== index) //Pour updater l'array qui contient les checked task et que ça se supprime comme les tasks non barrés
     setTasks(deleteTask);
+    setcheckedTasks(deleteCheckedTasks);
   }
 
-  //Pour modifier le task dans un input au même endroit
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [editedTask, setEditedTask] = useState("");
 
+  //editingIndex state revient null et donc fait apparaitre le stylo, la poubelle et le task précédemment écrit
   const handleCancel = () => {
     setEditingIndex(null)
   }
 
-  const updatedTasks = [...tasks];
 
+  //Pour éditer les tasks
   const handleUpdate = (index) => {
-    updatedTasks[index] = editedTask;
+    updatedTasks[index] = editedTask; //element ciblé va être value de la state editedTask
     setTasks(updatedTasks);
     setEditingIndex(null); //Pour revenir au stylo et poubelle quand on clique sur update
     setEditedTask(""); //Pour que quand on va encore update, ça revient pas à la valeur d'avant
   };
 
+  //Pour pouvoir barré chaque task sans barrer toutes les autres en même temps
+  // Dans handleclicked: on a donné à chaque element la property checked: false par défaut; quand on clique sur save task
+  const handlecheckedTasks = (index) => {
+    const updatedCheckedTasks = [...checkedTasks]; //crée une copie du tableau checkedTasks et store dans cette variable, pour ne pas changer le tableau d'origine lors de modification
+    updatedCheckedTasks[index] = !updatedCheckedTasks[index]; //false devient true quand on clique / On inverse chaque valeur contenu dans la copie de l'array checkedTasks, stocké dans updatedCheckedTasks / 
+    setcheckedTasks(updatedCheckedTasks);
+  }
+
+  //Comme setcheckedTasks est asynchronous, pour voir l'update du state, il faut faire le log dans useEffect
+  // useEffect(() => {
+  //   console.log(checkedTasks);
+  // }, [checkedTasks])
+
   return (
     <div className="all-todo-list">
       <h1>My To do List</h1>
-{/* Début map sur array stored dans task */}
-      {clicked && tasks.map((element, index) =>
-        <div className="my-task">
-          {editingIndex == index ?
-            <input
-              value={editedTask}
-              onChange={(e) => setEditedTask(e.target.value)}
-              placeholder={element}
-            />
-            :
-            <span className="the-task">{element}</span>
-          }
-          <div className="pen-trash">
-            {editingIndex == index ?
-              <div>
-                <button onClick={() => handleUpdate(index)}>Update</button>
-                <button onClick={handleCancel}>Cancel</button>
-              </div>
-              :
-              <div>
-                <FaRegPenToSquare onClick={() => setEditingIndex(index)} />
-                <FaRegTrashAlt onClick={() => eraseTask(index)} />
-              </div>
-            }
-          </div>
-        </div>
-      )}
-      {/* Fin map dans array stored dans tasl */}
+      {/* Début map sur array stored dans task */}
 
       <p>Tasks: </p>
       <input type="text" value={value} onChange={handleValue} />
-      <button className="btn-task" onClick={handleClicked}>Save Task</button>
-      {/* onClick={handleClicked} appelle la function quand on click, car on n'ajoute pas de paramètre */}
+      <button className="btn-task" onClick={handleClicked}>Save Task</button> {/* onClick={handleClicked} appelle la function quand on click, car on n'ajoute pas de paramètre */}
+
+
+      {clicked && tasks.map((element, index) =>
+        
+          <div key={index} className="my-task">
+            {editingIndex == index ?
+              <input
+                value={editedTask}
+                onChange={(e) => setEditedTask(e.target.value)}
+                placeholder={element}
+              />
+              :
+              <span className={`the-task ${checkedTasks[index] ? 'task-barré' : 'the-task'}`} >{element}</span>
+            }
+
+            <div className="pen-trash">
+              {editingIndex == index ?
+                <div>
+                  <button onClick={() => handleUpdate(index)}>Update</button>
+                  <button onClick={handleCancel}>Cancel</button>
+                </div>
+                :
+                <div>
+                  <input type="checkbox" onClick={()=>handlecheckedTasks(index)}></input>
+                  <FaRegPenToSquare onClick={() => setEditingIndex(index)} />
+                  <FaRegTrashAlt onClick={() => eraseTask(index)} />
+                </div>
+              }
+            </div>
+          </div>
+        )}
+      {/* Fin map dans array stored dans task */}
+
 
     </div>
   )
